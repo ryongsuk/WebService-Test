8fb180d57d909cdd6dc8e5dd01954fa6c1ac342a
vincent <ryongsuk@gmail.com>
2019-07-26 11:46:17 +0900

    Create Init code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo" />
+      <h2>Let's enjoy development!</h2>
+
+    </div>
+  );
+}
+
+export default App;
