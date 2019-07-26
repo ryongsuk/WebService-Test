edf44d866fa0f0dc231f4a58808864c35ecc9683
vincent <ryongsuk@gmail.com>
2019-07-26 17:50:36 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name':'홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name':'김포숙',
+    'birthday': '950219',
+    'gender': '여자',
+    'job': '회사'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name':'박폴',
+    'birthday': '910911',
+    'gender': '남자',
+    'job': '개발자'
+  },
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's enjoy development! by vincent</h2>
-
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
